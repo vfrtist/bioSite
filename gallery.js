a09f9d5edafa542e9d60e04427642127be488ea0
vfrtist <137813892+vfrtist@users.noreply.github.com>
2024-09-08 22:00:39 -0400

    first draft of the assignment

--- gallery.js
+++ gallery.js
@@ -1,8 +1,21 @@
-import fs from 'fs';
+const main = document.querySelector('#gallery');
 
-const main = document.querySelector('main');
-
-for (let item of fs.readdir('./images/')) {
-    console.log(item);
-}
 function make(item) { return document.createElement(item.toString()); }
+
+
+for (let card of main.querySelectorAll('.card')) {
+    card.addEventListener('click', (e) => {
+        const modal = make('dialog');
+        const image = card.querySelector('img').cloneNode('true');
+        modal.classList.add('modal');
+        modal.append(image);
+        document.body.append(modal);
+        modal.showModal();
+        modal.addEventListener('click', () => { modal.remove(); })
+    })
+}
+
+// magnify() {
+//     image.style.removeProperty('width');
+//     image.style.removeProperty('height');
+//     image.style.removeProperty('rotate');
